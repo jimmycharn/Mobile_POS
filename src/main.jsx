633a30438d13f25040fbfbc140d0daa3812725af
jimmycharn <>
2026-05-14 08:38:07 +0700

    add-pwa-support-for-mobile-install

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { registerSW } from 'virtual:pwa-register'
 import App from './App'
 import './index.css'
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm('มีเวอร์ชันใหม่ ต้องการรีเฟรชหรือไม่?')) {
+      updateSW(true)
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
